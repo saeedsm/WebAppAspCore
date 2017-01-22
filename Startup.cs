2319299b6cc4fce799faeae30fc08a502b5b0eac
Xiangdong Zhang <zhangxd6@Xiang.local>
2017-01-21 23:08:49 -0600

    middle ware run/use

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
 
 namespace DotNetGroupTalk
 {
@@ -28,8 +29,14 @@ namespace DotNetGroupTalk
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
-
+            
+            app.Use(async (context,next)=>{
+                 context.Response.Headers.Add("Middle","Hello MiddleWare");
+                await next.Invoke();
+            });
             app.UseStaticFiles();
+
+            
         }
     }
 
